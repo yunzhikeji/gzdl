5726bcd469664df9f50b00a336ff8d969742e433
wangqing <408564126@qq.com>
2016-06-16 08:56:03 +0800

    页面框架搭建

--- src/com/yz/controller/ItemsController.java
+++ src/com/yz/controller/ItemsController.java
@@ -175,7 +175,7 @@ public class ItemsController {
 		if(items_pic!=null && originalFilename!=null && originalFilename.length()>0){
 			
 			//存储图片的物理路径
-			String pic_path = "F:\\develop\\upload\\temp\\";
+			String pic_path = "D:\\develop\\upload\\temp\\";
 			
 			
 			//新的图片名称
