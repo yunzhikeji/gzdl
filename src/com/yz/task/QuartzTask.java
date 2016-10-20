fe62054caf0844a0325ea4760581bffe3e302a24
lq1997127 <477937204@qq.com>
2016-10-20 12:29:01 +0800

    定时处理设备经纬度

---
+++ src/com/yz/task/QuartzTask.java
@@ -0,0 +1,19 @@
+package com.yz.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yz.service.CameraService;
+
+public class QuartzTask {
+	
+	@Autowired
+	private CameraService cameraService;
+	
+	
+	public void doJob() {
+		// TODO Auto-generated method stub
+		System.out.println("hello world~");
+	}
+
+
+}
