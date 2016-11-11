0ea379194714dd1a022a323407a2cbcb0e0c5a2c
yuying1982 <qi.zhang@onecloudtech.cn>
2016-11-11 16:25:55 +0800

    展览视频单页

--- src/com/yz/controller/MapController.java
+++ src/com/yz/controller/MapController.java
@@ -1,11 +1,18 @@
 package com.yz.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.yz.po.Camera;
+import com.yz.service.CameraService;
+
 @Controller
 public class MapController {
+	
+	@Autowired
+	private CameraService cameraService;
 	
 	// 显示测试页面
 	@RequestMapping("/test")
@@ -24,6 +31,16 @@ public class MapController {
 	public String showMap(ModelMap map) throws Exception {
 		
 		return "map";
+	}
+	
+	
+	//显示展厅页面
+	@RequestMapping("/zt")
+	public String showZt(ModelMap map) throws Exception {
+		
+		Camera camera = cameraService.findCameraById(16);
+		map.put("camera", camera);
+		return "exhibitionVideo";
 	}
 	
 
